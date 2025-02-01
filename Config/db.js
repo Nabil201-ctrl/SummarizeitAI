1e94404d5e9fe9594b68d11ba960f8ac7d16d08c
Nabil201-ctrl <abubakar.nabil.210@gmail.com>
2025-02-01 13:50:37 +0100

    update the mongoose, to be more readable.

--- Config/db.js
+++ Config/db.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB database
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error: ', err));
 
